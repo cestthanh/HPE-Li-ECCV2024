ef86bfd446c0aeac107cfdc53f7761383ab15e93
NguyenTuanThanh <thanh1qaz7ujm@gmail.com>
2026-06-03 15:43:09 +0700

    Add WiPose dataset inspection scaffold

--- dataset_lib/__init__.py
+++ dataset_lib/__init__.py
@@ -6,4 +6,5 @@ Created on Mon Oct 21 09:03:07 2024
 @author: jackson-devworks
 """
 
-from .mmfi import make_dataloader, make_dataset
+from .factory import make_dataloader, make_dataset
+from .specs import DATASET_SPECS, get_dataset_spec
